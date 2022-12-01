4487e38ec04cae7da3830f245b79eff16123b184
Zheng Kai <zhengkai@gmail.com>
2022-12-01 15:00:15 +0800

    eslint

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styles: []
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styles: [],
 })
 export class AppComponent {
-  title = 'SearchUI';
+	title = 'SearchUI';
 }

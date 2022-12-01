fbd3ae1e701e5ae8e879072aee1bfeee5c273277
Zheng Kai <zhengkai@gmail.com>
2022-12-01 23:07:31 +0800

    fix button style

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,13 +7,57 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { FormsModule } from '@angular/forms';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatInputModule } from '@angular/material/input';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 import { SearchComponent } from './search/search.component';
 import { SettingComponent } from './setting/setting.component';
+
+// import { A11yModule } from '@angular/cdk/a11y';
+// import { CdkAccordionModule } from '@angular/cdk/accordion';
+// import { ClipboardModule } from '@angular/cdk/clipboard';
+// import { DragDropModule } from '@angular/cdk/drag-drop';
+// import { PortalModule } from '@angular/cdk/portal';
+// import { ScrollingModule } from '@angular/cdk/scrolling';
+// import { CdkStepperModule } from '@angular/cdk/stepper';
+// import { CdkTableModule } from '@angular/cdk/table';
+// import { CdkTreeModule } from '@angular/cdk/tree';
+// import { MatBadgeModule } from '@angular/material/badge';
+// import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
+// import { MatButtonToggleModule } from '@angular/material/button-toggle';
+// import { MatCardModule } from '@angular/material/card';
+// import { MatCheckboxModule } from '@angular/material/checkbox';
+// import { MatChipsModule } from '@angular/material/chips';
+// import { MatStepperModule } from '@angular/material/stepper';
+// import { MatDatepickerModule } from '@angular/material/datepicker';
+// import { MatDialogModule } from '@angular/material/dialog';
+// import { MatDividerModule } from '@angular/material/divider';
+// import { MatExpansionModule } from '@angular/material/expansion';
+// import { MatGridListModule } from '@angular/material/grid-list';
+// import { MatListModule } from '@angular/material/list';
+// import { MatMenuModule } from '@angular/material/menu';
+// import { MatNativeDateModule, MatRippleModule } from '@angular/material/core';
+// import { MatPaginatorModule } from '@angular/material/paginator';
+// import { MatProgressBarModule } from '@angular/material/progress-bar';
+// import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+// import { MatRadioModule } from '@angular/material/radio';
+// import { MatSelectModule } from '@angular/material/select';
+// import { MatSidenavModule } from '@angular/material/sidenav';
+// import { MatSliderModule } from '@angular/material/slider';
+// import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+// import { MatSnackBarModule } from '@angular/material/snack-bar';
+// import { MatSortModule } from '@angular/material/sort';
+// import { MatTableModule } from '@angular/material/table';
+// import { MatTabsModule } from '@angular/material/tabs';
+// import { MatTooltipModule } from '@angular/material/tooltip';
+// import { MatTreeModule } from '@angular/material/tree';
+// import { OverlayModule } from '@angular/cdk/overlay';
+// import { CdkMenuModule } from '@angular/cdk/menu';
+// import { DialogModule } from '@angular/cdk/dialog';
 
 @NgModule({
 	declarations: [
@@ -25,15 +69,63 @@ import { SettingComponent } from './setting/setting.component';
 		BrowserModule,
 		RoutingModule,
 		NoopAnimationsModule,
+		FormsModule,
+		MatFormFieldModule,
+
 		MatToolbarModule,
 		MatInputModule,
-		MatAutocompleteModule,
 		MatIconModule,
-		FormsModule,
+		MatAutocompleteModule,
+		MatButtonModule,
 	],
 	providers: [],
 	bootstrap: [
 		BootstrapComponent,
 	],
 })
-export class AppModule { }
+export class AppModule {
+	ignore = [
+		// A11yModule,
+		// CdkAccordionModule,
+		// CdkMenuModule,
+		// CdkStepperModule,
+		// CdkTableModule,
+		// CdkTreeModule,
+		// ClipboardModule,
+		// DialogModule,
+		// DragDropModule,
+		// MatBadgeModule,
+		// MatBottomSheetModule,
+		// MatButtonToggleModule,
+		// MatCardModule,
+		// MatCheckboxModule,
+		// MatChipsModule,
+		// MatDatepickerModule,
+		// MatDialogModule,
+		// MatDividerModule,
+		// MatExpansionModule,
+		// MatGridListModule,
+		// MatListModule,
+		// MatMenuModule,
+		// MatNativeDateModule,
+		// MatPaginatorModule,
+		// MatProgressBarModule,
+		// MatProgressSpinnerModule,
+		// MatRadioModule,
+		// MatRippleModule,
+		// MatSelectModule,
+		// MatSidenavModule,
+		// MatSlideToggleModule,
+		// MatSliderModule,
+		// MatSnackBarModule,
+		// MatSortModule,
+		// MatStepperModule,
+		// MatTableModule,
+		// MatTabsModule,
+		// MatTooltipModule,
+		// MatTreeModule,
+		// OverlayModule,
+		// PortalModule,
+		// ScrollingModule,
+	];
+}

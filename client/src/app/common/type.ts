63f44026af0eff9f6b3fc9c21f11c8ac3c8b41ad
Zheng Kai <zhengkai@gmail.com>
2022-12-05 10:00:37 +0800

    up

---
+++ client/src/app/common/type.ts
@@ -0,0 +1,12 @@
+export type SearchKey = 'name' | 'author' | 'publisher' | 'lang' | 'ext' | 'isbn';
+
+export const KeyName = {
+	name: '书名',
+	author: '作者',
+	publisher: '出版社',
+	lang: '语言',
+	ext: '扩展名',
+	isbn: 'ISBN',
+	zlib_id: 'zLib ID',
+	page: '页码',
+};
--- client/src/app/setting/setting.component.ts
+++ client/src/app/setting/setting.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavService } from '../common/nav.service';
+import { KeyName } from '../common/type';
 
 @Component({
 	selector: 'app-setting',
@@ -9,7 +10,34 @@ import { NavService } from '../common/nav.service';
 })
 export class SettingComponent {
 
+	rememberLastSearch = false;
+	dlLeftButton = false;
+
+	searchCol = {
+		lang: false,
+		publisher: false,
+		ext: false,
+		isbn: false,
+		zlib_id: false,
+	};
+
+	resultCol = {
+		lang: false,
+		publisher: false,
+		ext: false,
+		isbn: false,
+		zlib_id: false,
+		page: false,
+	};
+
+	fileName = {
+		publisher: false,
+		author: false,
+	};
+
 	dlSite = 'https://cloudflare-ipfs.com/';
+
+	keyName: any = KeyName;
 
 	dlSiteSuggest = [
 		'https://cloudflare-ipfs.com/',
